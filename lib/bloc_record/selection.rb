743d5927333e8e2d60d57bca8e851ad51d6f5d7b
Lisa Min <lisajmin@gmail.com>
2018-05-23 23:53:07 -0700

    Checkpoint 2: SELECT methods

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -1,13 +1,155 @@
 require 'sqlite3'
 
 module Selection
-  def find(id)
+  def find(*ids)
+    ids.each do |id|
+      validate_integer(id)
+    end
+
+    if ids.length == 1
+      find_one(ids.first)
+    else
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE id IN (#{ids.join(",")});
+      SQL
+
+      rows_to_array(rows)
+    end
+  end
+
+  def find_one(id)
+    validate_integer(id)
+
     row = connection.get_first_row <<-SQL
     SELECT #{columns.join ","} FROM #{table}
     WHERE id = #{id};
     SQL
 
-    data = Hash[columns.zip(row)]
-    new(data)
+    init_object_from_row(row)
+  end
+
+  def find_by(attribute, value)
+    validate_string(attribute)
+    validate_string(value)
+
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
+    SQL
+
+    rows_to_array(rows)
+  end
+
+  def find_each(options = {})
+    start = options[:start] || 0
+    batch_size = options[:batch_size] || 1000
+
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      LIMIT #{batch_size} OFFSET #{start};
+    SQL
+
+    rows.each do |row|
+      yield init_object_from_row(row)
+    end
+  end
+
+  def find_in_batches(options = {})
+    start = options[:start] || 0
+    batch_size = options[:batch_size] || 1000
+
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      LIMIT #{batch_size} OFFSET #{start};
+    SQL
+
+    yield(rows_to_array(rows))
+  end
+
+  def method_missing(m, *args)
+    attribute = m.to_s
+    attribute.slice!("find_by_")
+    find_by(attribute, args[0])
+  end
+
+  def take(num=1)
+    validate_integer(num)
+
+    if num > 1
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        ORDER BY random()
+        LIMIT #{num};
+      SQL
+
+      rows_to_array(rows)
+    else
+      take_one
+    end
+  end
+
+  def take_one
+    row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      ORDER BY random()
+      LIMIT 1;
+    SQL
+
+    init_object_from_row(row)
+  end
+
+  def first
+    row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      ORDER BY id ASC LIMIT 1;
+    SQL
+
+    init_object_from_row(row)
+  end
+
+  def last
+    row = connection.get_first_row <<-SQL
+      SELEC #{columns.join ","} FROM #{table}
+      ORDER BY id DESC LIMIT 1;
+    SQL
+
+    init_object_from_row(row)
+  end
+
+  def all
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table};
+    SQL
+
+    rows_to_array(rows)
+  end
+
+  private
+
+  def init_object_from_row(row)
+    if row
+      data = Hash[columns.zip(row)]
+      new(data)
+    end
+  end
+
+  def rows_to_array(rows)
+    rows.map { |row| new(Hash[columns.zip(row)]) }
+  end
+
+  def validate_integer(number)
+    if !number.is_a?(Integer)
+      raise ArgumentError, "Error: #{number} must be an integer."
+    end
+    if number < 1
+      raise ArgumentError, "Error: #{number} must be greater than 0."
+    end
+  end
+
+  def validate_string(word)
+    if !word.is_a?(String)
+      raise ArgumentError, "Error: #{word} must be a string."
+    end
   end
 end
